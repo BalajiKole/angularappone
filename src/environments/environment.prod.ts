91d88c0c13e2f49768ee932eacb38b5dbe6f6ecf
BalajiKole <tqbalaji@gmail.com>
2021-09-29 11:38:45 +0530

    first commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  ServerUrl:'https://zonionproject.herokuapp.com/data',
+ServerUrlOne:"https://zonionproject.herokuapp.com/data/updaterestimage"
+};
